ac26e2e08fce2e92f00723065993e750a6dbffd6
Manik patra <72035800+patramanik@users.noreply.github.com>
2025-04-06 19:24:54 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -11,11 +11,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'));
 
+// Middleware to log requests and responses
 app.use(logReqRes("log.txt"));
 
+// define your routes here
 app.get("/", (req, res) => {
-  res.send("Welcome to the backend API!");
+  res.status(200).send("Hello World!");
 });
+
+app.get("/home", (req, res) => {
+   const id = req.query.id;
+//   req.params;
+//   console.log(req.params); // { id: 1 }
+//   console.log(req.body); // { name: "John" }
+//   req.headers;
+  console.log(req.query); // { id: 1 }
+  // req.query = { name: "John" };
+  // req.body = { age: 30 };
+  const name = "John Doe"; // Example variable
+  res.json({ message: "Welcome to the home page!", name, id });
+});
+
+// app.use("/api/home", );
 
 // Start the server
 app.listen(PORT, () => {
---
+++ backend/setap.md
@@ -0,0 +1,38 @@
+# Node js Project Setap
+
+#### git comments
+```bash
+    git status
+    git add index.js 
+    # filename.js
+    git add .
+
+    git commit -m "hi"
+    # for push on current branch 
+    git push 
+    #for push on main or any other branch 
+    git push origin main
+
+    # for pull on current branch 
+    git pull 
+    
+    # for pull from any branch
+    git pull origin manik
+
+```
+
+## Init server
+``` bash
+    <!-- for create project -->
+    npm init 
+
+```
+
+## Install package 
+``` bash
+    npm i express 
+    npm i dotenv
+    npm install --save-dev nodemon
+    npm i cors
+    
+```
